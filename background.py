8b7c98039cf293f410caa409c8a702cf03a7eb89
AlexisEpsi <152502075+AlexisEpsi@users.noreply.github.com>
2024-01-25 11:32:19 +0100

    Update background.py

---
+++ background.py
@@ -0,0 +1,14 @@
+import ctypes
+import os
+import requests
+
+SPI_SETDESKWALLPAPER = 0x0014
+
+def change_wallpaper(file_path):
+    file_path = os.path.abspath(file_path)
+    file_path = file_path.encode('utf-16le') + b'\x00\x00'
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, file_path, 3)
+
+if __name__ == "__main__":
+    local_path = './image.png'
+    change_wallpaper(local_path)
