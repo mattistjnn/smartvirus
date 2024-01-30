f9b1e9f1144f72ac07cfaf03ad3033312c399d62
mattis_tjn <mattis.tajan@ecoles-epsi.net>
2024-01-30 15:15:34 +0100

    tout remis

---
+++ background.py
@@ -0,0 +1,13 @@
+import ctypes
+import os
+
+SPI_SETDESKWALLPAPER = 0x0014
+
+def change_wallpaper(file_path):
+    file_path = os.path.abspath(file_path)
+    file_path = file_path.encode('utf-16le') + b'\x00\x00'
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, file_path, 3)
+
+if __name__ == "__main__":
+    local_path = './chocoblast.png'
+    change_wallpaper(local_path)
